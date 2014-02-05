a3d3405c28e8b79017c57170fc4fa4b2478853fe
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-02-05 16:34:48 +0100

    Move model code in triangle.go

--- gl/gl.go
+++ gl/gl.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 
 	"github.com/go-gl/gl"
 	glfw "github.com/go-gl/glfw3"
@@ -18,12 +17,11 @@ func errorCallback(err glfw.ErrorCode, desc string) {
 	fmt.Printf("Error callback %v: %v\n", err, desc)
 }
 
-var vertices = []float32{-0.5, 0.0, 0.5, 0.5, 0.5, -0.5, // triange 1
-	-0.8, -0.8, -0.3, -0.8, -0.8, -0.3} // Triangle 2
-var colors = []float32{1.0, 0, 0}
+var vertices = [...]float32{-0.5, 0.0, 0.5, 0.5, 0.5, -0.5} // triange 1
+//	-0.8, -0.8, -0.3, -0.8, -0.8, -0.3} // Triangle 2
+var colors = [...]float32{1.0, 0, 0}
 
-var prg gl.Program
-var buffer gl.Buffer
+var triangle *Triangle
 
 func main() {
 	glfw.SetErrorCallback(errorCallback)
@@ -33,12 +31,11 @@ func main() {
 	}
 	defer glfw.Terminate()
 
-	//showVersion()
-	//glfw.WindowHint(glfw.ContextVersionMajor, 3)
-	//glfw.WindowHint(glfw.ContextVersionMinor, 3)
-	//glfw.WindowHint(glfw.OpenglForwardCompatible, glfw.True)
-	//glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
-	//showVersion()
+	showVersion()
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenglForwardCompatible, glfw.True)
+	glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
 
 	window, err := glfw.CreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, TITLE, nil, nil)
 	if err != nil {
@@ -50,7 +47,7 @@ func main() {
 	glfw.SwapInterval(1)
 
 	gl.Init()
-	//initScene()
+	initScene()
 	defer destroyScene()
 
 	for !window.ShouldClose() {
@@ -65,72 +62,38 @@ func showVersion() {
 	fmt.Println("version=", glfw.GetVersionString())
 }
 
-func initScene() error {
-	buffer = gl.GenBuffer()
-	buffer.Bind(gl.ARRAY_BUFFER)
-	var f float32
-	floatSize := int(unsafe.Sizeof(f))
-	sizeVert := len(vertices) * floatSize
-	sizeColors := len(colors) * floatSize
-
-	gl.BufferData(gl.ARRAY_BUFFER, sizeVert+sizeColors, nil, gl.STATIC_DRAW)
-	gl.BufferSubData(gl.ARRAY_BUFFER, 0, sizeVert, vertices)
-	gl.BufferSubData(gl.ARRAY_BUFFER, sizeVert, sizeColors, vertices)
-	buffer.Unbind(gl.ARRAY_BUFFER)
-
-	//gl.Viewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-	//gl.MatrixMode(gl.PROJECTION)
-	vs := `
-	#version 330
-	in vec3 in_vertex;
-
-		void main() {
-			gl_Position = vec4(in_vertex, 0.0);
-				}
-				`
-	fs := `
-	#version 330
-	out vec4 out_color;
-
-		void main() {
-			out_color= vec4(1.0,1.0,1.0,1.0);
-				}
-				`
-	vshader := gl.CreateShader(gl.VERTEX_SHADER)
-	vshader.Source(vs)
-	vshader.Compile()
-	if vshader.Get(gl.COMPILE_STATUS) != gl.TRUE {
-		panic("vertex shader error :" + vshader.GetInfoLog())
-	}
-	fshader := gl.CreateShader(gl.FRAGMENT_SHADER)
-	fshader.Source(fs)
-	fshader.Compile()
-	if fshader.Get(gl.COMPILE_STATUS) != gl.TRUE {
-		panic("fragment shader error:" + fshader.GetInfoLog())
-	}
-
-	prg = gl.CreateProgram()
-	//prg.AttachShader(vshader)
-	//prg.AttachShader(fshader)
-	//prg.BindAttribLocation(0, "in_vertex")
-	//prg.BindAttribLocation(1, "out_color")
-	prg.Link()
-	if prg.Get(gl.LINK_STATUS) != gl.TRUE {
-		panic("linker error: " + prg.GetInfoLog())
-	}
-
-	return nil
-}
-
-func destroyScene() {
-	prg.Delete()
+func initScene() {
+	gl.ClearColor(0.0, 0.0, 0.0, 1.0)
+	triangle = NewTriangle(vertices, colors)
+	triangle.Load()
 }
 
 func drawScene() {
 	gl.Clear(gl.COLOR_BUFFER_BIT)
-	//prg.Use()
-	gl.EnableClientState(gl.VERTEX_ARRAY)
-	gl.VertexPointer(2, gl.FLOAT, 0, vertices)
-	gl.DrawArrays(gl.TRIANGLES, 0, 6)
-	gl.DisableClientState(gl.VERTEX_ARRAY)
+	triangle.Draw()
+}
+
+func loadShader(type_ gl.GLenum, source string) gl.Shader {
+	shader := gl.CreateShader(type_)
+	shader.Source(source)
+	shader.Compile()
+	if shader.Get(gl.COMPILE_STATUS) != gl.TRUE {
+		panic("fragment error for source " + source + "\n" + shader.GetInfoLog())
+	}
+	return shader
+}
+
+func attachShaders(prg gl.Program, shaders ...gl.Shader) {
+	for _, shader := range shaders {
+		prg.AttachShader(shader)
+	}
+	prg.Link()
+	if prg.Get(gl.LINK_STATUS) != gl.TRUE {
+		panic("linker error: " + prg.GetInfoLog())
+	}
+	prg.Validate()
+}
+
+func destroyScene() {
+	triangle.Destroy()
 }
---
+++ gl/triangle.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"unsafe"
+
+	"github.com/go-gl/gl"
+)
+
+const shaderVert = `
+#version 330
+
+layout(location=0) in vec4 position;
+layout(location=1) in vec4 color;
+
+out vec4 fragColor;
+
+void main() {
+	gl_Position = position;
+	fragColor = color;
+}`
+
+const shaderFrag = `
+#version 330
+
+in vec4 fragColor;
+out vec4 outColor;
+
+void main() {
+  outColor = fragColor;
+}`
+
+type Triangle struct {
+	buffer       gl.Buffer
+	vertices     []float32
+	colors       []float32
+	sizeVertices int
+	sizeColors   int
+	prg          gl.Program
+	posLoc       gl.AttribLocation
+	colLoc       gl.AttribLocation
+}
+
+func NewTriangle(vertices [6]float32, colors [3]float32) *Triangle {
+	t := &Triangle{}
+	t.vertices = []float32{-0.5, 0.0, 0.5, 0.5, 0.5, -0.5}
+	t.colors = []float32{1.0, 0, 0, 1.0}
+
+	var f float32
+	floatSize := int(unsafe.Sizeof(f))
+	t.sizeVertices = len(t.vertices) * floatSize
+	t.sizeColors = len(t.colors) * floatSize
+
+	vshader := loadShader(gl.VERTEX_SHADER, shaderVert)
+	fshader := loadShader(gl.FRAGMENT_SHADER, shaderFrag)
+
+	t.prg = gl.CreateProgram()
+	attachShaders(t.prg, vshader, fshader)
+	t.prg.BindAttribLocation(0, "position")
+	t.prg.BindAttribLocation(1, "color")
+	t.posLoc = t.prg.GetAttribLocation("position")
+	t.colLoc = t.prg.GetAttribLocation("color")
+	return t
+}
+
+func (t *Triangle) Load() {
+	t.buffer.Delete()
+	t.buffer = gl.GenBuffer()
+	t.buffer.Bind(gl.ARRAY_BUFFER)
+
+	gl.BufferData(gl.ARRAY_BUFFER, t.sizeColors+t.sizeVertices, nil, gl.STATIC_DRAW)
+	gl.BufferSubData(gl.ARRAY_BUFFER, 0, t.sizeVertices, t.vertices)
+	gl.BufferSubData(gl.ARRAY_BUFFER, t.sizeVertices, t.sizeColors, t.colors)
+
+	t.posLoc.EnableArray()
+	t.colLoc.EnableArray()
+	t.posLoc.AttribPointer(2, gl.FLOAT, false, 0, uintptr(0))
+	t.colLoc.AttribPointer(4, gl.FLOAT, false, 0, uintptr(t.sizeVertices))
+	t.posLoc.DisableArray()
+	t.colLoc.DisableArray()
+
+	t.buffer.Unbind(gl.ARRAY_BUFFER)
+
+}
+
+func (t *Triangle) Draw() {
+	t.prg.Use()
+	t.buffer.Bind(gl.ARRAY_BUFFER)
+	t.posLoc.EnableArray()
+	t.colLoc.EnableArray()
+
+	gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+	t.posLoc.DisableArray()
+	t.colLoc.DisableArray()
+	t.buffer.Unbind(gl.ARRAY_BUFFER)
+	gl.ProgramUnuse()
+}
+
+func (t *Triangle) Destroy() {
+	t.buffer.Delete()
+}
