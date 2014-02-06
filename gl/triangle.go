f32a6fb581fea0196947075733610652684a07de
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-02-06 15:30:39 +0100

    color in vexter data

--- gl/triangle.go
+++ gl/triangle.go
@@ -7,38 +7,31 @@ import (
 )
 
 type Triangle struct {
-	buffer       gl.Buffer
-	vertices     []float32
-	colors       []float32
-	sizeVertices int
-	sizeColors   int
-	prg          gl.Program
-	posLoc       gl.AttribLocation
-	colLoc       gl.AttribLocation
-	vao          gl.VertexArray
+	buffer     gl.Buffer
+	vertexData []float32
+	sizeData   int
+	prg        gl.Program
+	posLoc     gl.AttribLocation
+	colLoc     gl.AttribLocation
+	vao        gl.VertexArray
 }
 
-func NewTriangle(vertices []float32) *Triangle {
+func NewTriangle(vertexData []float32) *Triangle {
 	t := &Triangle{}
-	t.vertices = vertices
+	t.vertexData = vertexData
+	t.sizeData = len(t.vertexData) * int(unsafe.Sizeof(float32(0)))
 
-	floatSize := int(unsafe.Sizeof(0))
-	t.sizeVertices = len(t.vertices) * floatSize
-	t.sizeColors = len(t.colors) * floatSize
-
-	vshader := loadShader(gl.VERTEX_SHADER, "shaders/basic.vert")
-	fshader := loadShader(gl.FRAGMENT_SHADER, "shaders/colory.frag")
-
+	vshader := loadShader(gl.VERTEX_SHADER, "shaders/colorv.vert")
+	fshader := loadShader(gl.FRAGMENT_SHADER, "shaders/colorv.frag")
 	t.prg = NewProgram(vshader, fshader)
 	t.posLoc = gl.AttribLocation(0)
+	t.colLoc = gl.AttribLocation(1)
 
-	//t.buffer.Delete()
 	t.buffer = gl.GenBuffer()
 	t.buffer.Bind(gl.ARRAY_BUFFER)
 
-	gl.BufferData(gl.ARRAY_BUFFER, t.sizeVertices, nil, gl.STATIC_DRAW)
-	gl.BufferSubData(gl.ARRAY_BUFFER, 0, t.sizeVertices, t.vertices)
-	//gl.BufferSubData(gl.ARRAY_BUFFER, t.sizeVertices, t.sizeColors, t.colors)
+	gl.BufferData(gl.ARRAY_BUFFER, t.sizeData, nil, gl.STATIC_DRAW)
+	gl.BufferSubData(gl.ARRAY_BUFFER, 0, t.sizeData, t.vertexData)
 
 	t.buffer.Unbind(gl.ARRAY_BUFFER)
 
@@ -51,10 +44,12 @@ func (t *Triangle) Draw() {
 	t.prg.Use()
 	t.buffer.Bind(gl.ARRAY_BUFFER)
 
-	t.posLoc.AttribPointer(4, gl.FLOAT, false, 0, uintptr(0))
 	t.posLoc.EnableArray()
+	t.posLoc.AttribPointer(4, gl.FLOAT, false, 0, uintptr(0))
+	t.colLoc.EnableArray()
+	t.colLoc.AttribPointer(4, gl.FLOAT, false, 0, uintptr(48))
 
-	//t.colLoc.AttribPointer(4, gl.FLOAT, false, 0, uintptr(t.sizeVertices))
+	//t.colLoc.AttribPointer(4, gl.FLOAT, false, 0, uintptr(t.sizeData))
 	gl.DrawArrays(gl.TRIANGLES, 0, 3)
 
 	t.posLoc.DisableArray()
